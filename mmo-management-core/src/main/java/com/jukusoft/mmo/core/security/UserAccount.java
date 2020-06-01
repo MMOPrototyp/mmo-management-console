8b94b8bbb4b7803361d4c662ae8897cd7fd904e1
JuKu <kuenzel.justin@t-online.de>
2020-06-01 20:01:48 +0200

    Improve software quality for sonarqube

--- mmo-management-core/src/main/java/com/jukusoft/mmo/core/security/UserAccount.java
+++ mmo-management-core/src/main/java/com/jukusoft/mmo/core/security/UserAccount.java
@@ -4,6 +4,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
+import java.util.Objects;
 
 public class UserAccount extends UsernamePasswordAuthenticationToken {
 
@@ -25,6 +26,20 @@ public class UserAccount extends UsernamePasswordAuthenticationToken {
     @Override
     public boolean isAuthenticated() {
         return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserAccount)) return false;
+        if (!super.equals(o)) return false;
+        UserAccount that = (UserAccount) o;
+        return userID == that.userID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), userID);
     }
 
 }
