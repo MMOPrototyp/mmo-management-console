39f3f279083964ab78b42b32cfe81f63a76db653
JuKu <kuenzel.justin@t-online.de>
2020-06-01 20:07:24 +0200

    Improve software quality for sonarqube

--- mmo-management-main/src/main/java/com/jukusoft/mmo/main/config/WebSecurityConfig.java
+++ mmo-management-main/src/main/java/com/jukusoft/mmo/main/config/WebSecurityConfig.java
@@ -41,31 +41,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtAuthenticationEntryPoint unauthorizedHandler;
 
-    /*@Autowired
-    private JwtAuthenticationProvider jwtAuthenticationProvider;
-
-    @Autowired
-    public void configureAuthentication(AuthenticationManagerBuilder authenticationManagerBuilder) {
-        authenticationManagerBuilder.authenticationProvider(jwtAuthenticationProvider);
-    }*/
-
-    /*@Deprecated
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(jwtAuthenticationProvider);
-    }*/
-
-    /*@Bean
-    public JwtAuthenticationTokenFilter authenticationTokenFilterBean() {
-        return new JwtAuthenticationTokenFilter();
-    }*/
-
-    /*@Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }*/
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //disable CSRF for REST api, else REST client cannot connect to REST api
@@ -105,39 +80,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/pages/**", configuration);
         return source;
     }
-
-    /*@Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .exceptionHandling()
-                .authenticationEntryPoint(restAuthenticationEntryPoint)
-                .and()
-                .authorizeRequests()
-                //allow access to / and /login to any user (also unauthentificated ones)
-                .antMatchers("/login", "/actuator", "/actuator/*", "/errors/*", "/error", "/pages/*", "/res/**", "/avatar/**", "/api/auth/create-token", "/api/auth/verify-token", "/api/check-client-version", "/api/register")
-                .permitAll()
-                //lock out any unauthentificated users from any other page
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic()
-                .realmName(Config.get("Login", "realmName"))
-                //.formLogin()
-                //.successHandler(mySuccessHandler)
-                //.failureHandler(myFailureHandler)
-                .and()
-                //.exceptionHandling().accessDeniedPage("/errors/access-denied")
-                //.and()
-                .logout();
-    }*/
-
-    /*@Override
-    public void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-        //register own custom authentification provider
-        auth.authenticationProvider(authProvider);
-    }*/
 
     @Bean
     public PasswordEncoder encoder() {
