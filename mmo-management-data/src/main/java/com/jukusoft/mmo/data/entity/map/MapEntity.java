a9414f86547b6f064d8ce5ec580ab3b94f1992ee
JuKu <kuenzel.justin@t-online.de>
2020-06-04 12:11:20 +0200

    Add new MapEntity

---
+++ mmo-management-data/src/main/java/com/jukusoft/mmo/data/entity/map/MapEntity.java
@@ -0,0 +1,32 @@
+package com.jukusoft.mmo.data.entity.map;
+
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "game_maps", indexes = {
+        //@Index(columnList = "email", name = "email_idx"),
+}, uniqueConstraints = {
+        //@UniqueConstraint(columnNames = "username", name = "username_uqn")
+})
+@Cacheable//use second level cache
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class MapEntity {
+
+    @Size(min = 2, max = 45)
+    @Column(name = "name", unique = true, nullable = false, updatable = true)
+    @NotEmpty(message = "name is required")
+    private String name;
+
+    @Size(min = 2, max = 90)
+    @Column(name = "path", unique = true, nullable = false, updatable = true)
+    @NotEmpty(message = "path is required")
+    private String path;
+
+}
---
+++ mmo-management-data/src/main/java/com/jukusoft/mmo/data/entity/map/ZoneEntity.java
@@ -0,0 +1,30 @@
+package com.jukusoft.mmo.data.entity.map;
+
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+/**
+ * a zone contains one or many regions.
+ */
+@Entity
+@Table(name = "game_zones", indexes = {
+        //@Index(columnList = "email", name = "email_idx"),
+}, uniqueConstraints = {
+        //@UniqueConstraint(columnNames = "username", name = "username_uqn")
+})
+@Cacheable//use second level cache
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class ZoneEntity {
+
+    @Size(min = 2, max = 45)
+    @Column(name = "name", unique = true, nullable = false, updatable = true)
+    @NotEmpty(message = "name is required")
+    private String name;
+
+}
