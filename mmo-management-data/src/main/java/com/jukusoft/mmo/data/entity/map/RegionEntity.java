dd3d81ea7ba578687038d1874af0568e9d0bd942
JuKu <kuenzel.justin@t-online.de>
2020-06-06 17:11:45 +0200

    Change RegionEntity

--- mmo-management-data/src/main/java/com/jukusoft/mmo/data/entity/map/RegionEntity.java
+++ mmo-management-data/src/main/java/com/jukusoft/mmo/data/entity/map/RegionEntity.java
@@ -1,12 +1,10 @@
 package com.jukusoft.mmo.data.entity.map;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.jukusoft.mmo.data.entity.AbstractEntity;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import javax.persistence.Cacheable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
@@ -20,9 +18,31 @@ import javax.validation.constraints.Size;
 @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class RegionEntity extends AbstractEntity {
 
+    @ManyToOne(optional = false, cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "zone_id", nullable = false, updatable = false)//don't use an extra table, use join column instead
+    @JsonIgnore
+    private ZoneEntity zone;
+
     @Size(min = 2, max = 45)
     @Column(name = "name", unique = true, nullable = false, updatable = true)
     @NotEmpty(message = "name is required")
     private String name;
+
+    public RegionEntity(ZoneEntity zone, @Size(min = 2, max = 45) @NotEmpty(message = "name is required") String name) {
+        this.zone = zone;
+        this.name = name;
+    }
+
+    protected RegionEntity() {
+        //
+    }
+
+    public ZoneEntity getZone() {
+        return zone;
+    }
+
+    public String getName() {
+        return name;
+    }
 
 }
--- mmo-management-data/src/main/java/com/jukusoft/mmo/data/entity/map/ZoneEntity.java
+++ mmo-management-data/src/main/java/com/jukusoft/mmo/data/entity/map/ZoneEntity.java
@@ -3,12 +3,12 @@ package com.jukusoft.mmo.data.entity.map;
 import com.jukusoft.mmo.data.entity.AbstractEntity;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import javax.persistence.Cacheable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * a zone contains one or many regions.
@@ -28,16 +28,31 @@ public class ZoneEntity extends AbstractEntity {
     @NotEmpty(message = "name is required")
     private String name;
 
+    @OneToMany(mappedBy = "zone", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @Column(name = "zone")
+    private List<RegionEntity> regions;
+
     public ZoneEntity(@Size(min = 2, max = 45) @NotEmpty(message = "name is required") String name) {
         this.name = name;
+        this.regions = new ArrayList<>();
     }
 
-    public ZoneEntity() {
+    protected ZoneEntity() {
         //
     }
 
     public String getName() {
         return name;
+    }
+
+    public void addRegion(RegionEntity region) {
+        Objects.requireNonNull(region);
+        this.regions.add(region);
+    }
+
+    public void removeRegion(RegionEntity region) {
+        Objects.requireNonNull(region);
+        this.regions.remove(region);
     }
 
 }
