f44320eb65724f0355dcca2ecc6c40618712481e
JuKu <kuenzel.justin@t-online.de>
2020-06-06 18:17:24 +0200

    Change StatisticsService

--- mmo-management-service/src/main/java/com/jukusoft/mmo/service/statistics/StatisticsDTO.java
+++ mmo-management-service/src/main/java/com/jukusoft/mmo/service/statistics/StatisticsDTO.java
@@ -2,6 +2,32 @@ package com.jukusoft.mmo.service.statistics;
 
 public class StatisticsDTO {
 
-    //
+    protected long userCount;
+    protected long zoneCount;
+    protected long regionCount;
+    protected long mapCount;
 
+    public StatisticsDTO(long userCount, long zoneCount, long regionCount, long mapCount) {
+        this.userCount = userCount;
+        this.zoneCount = zoneCount;
+        this.regionCount = regionCount;
+        this.mapCount = mapCount;
+    }
+
+    public long getUserCount() {
+        return userCount;
+    }
+
+    public long getZoneCount() {
+        return zoneCount;
+    }
+
+    public long getRegionCount() {
+        return regionCount;
+    }
+
+    public long getMapCount() {
+        return mapCount;
+    }
+    
 }
--- mmo-management-service/src/main/java/com/jukusoft/mmo/service/statistics/StatisticsService.java
+++ mmo-management-service/src/main/java/com/jukusoft/mmo/service/statistics/StatisticsService.java
@@ -1,12 +1,29 @@
 package com.jukusoft.mmo.service.statistics;
 
+import com.jukusoft.mmo.data.dao.MapDAO;
+import com.jukusoft.mmo.data.dao.RegionDAO;
+import com.jukusoft.mmo.data.dao.UserDAO;
+import com.jukusoft.mmo.data.dao.ZoneDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StatisticsService {
 
+    @Autowired
+    private UserDAO userDAO;
+
+    @Autowired
+    private ZoneDAO zoneDAO;
+
+    @Autowired
+    private RegionDAO regionDAO;
+
+    @Autowired
+    private MapDAO mapDAO;
+
     public StatisticsDTO getStatistics() {
-        return new StatisticsDTO();
+        return new StatisticsDTO(userDAO.count(), zoneDAO.count(), regionDAO.count(), mapDAO.count());
     }
 
 }
