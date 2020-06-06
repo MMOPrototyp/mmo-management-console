39e56e350c965a05ea8dbc1990f2cdd6d7bdffc7
JuKu <kuenzel.justin@t-online.de>
2020-06-06 16:58:53 +0200

    Change MapImporterService

--- mmo-management-data/src/main/java/com/jukusoft/mmo/data/entity/map/ZoneEntity.java
+++ mmo-management-data/src/main/java/com/jukusoft/mmo/data/entity/map/ZoneEntity.java
@@ -24,8 +24,20 @@ import javax.validation.constraints.Size;
 public class ZoneEntity extends AbstractEntity {
 
     @Size(min = 2, max = 45)
-    @Column(name = "name", unique = true, nullable = false, updatable = true)
+    @Column(name = "name", unique = true, nullable = false, updatable = false)
     @NotEmpty(message = "name is required")
     private String name;
+
+    public ZoneEntity(@Size(min = 2, max = 45) @NotEmpty(message = "name is required") String name) {
+        this.name = name;
+    }
+
+    public ZoneEntity() {
+        //
+    }
+
+    public String getName() {
+        return name;
+    }
 
 }
--- mmo-management-service/src/main/java/com/jukusoft/mmo/service/setting/MapImporterService.java
+++ mmo-management-service/src/main/java/com/jukusoft/mmo/service/setting/MapImporterService.java
@@ -1,15 +1,29 @@
 package com.jukusoft.mmo.service.setting;
 
+import com.jukusoft.mmo.core.utils.FileUtils;
+import com.jukusoft.mmo.data.dao.ZoneDAO;
+import com.jukusoft.mmo.data.entity.map.ZoneEntity;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class MapImporterService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MapImporterService.class);
+
+    @Autowired
+    private ZoneDAO zoneDAO;
 
     @Transactional
     public void importDir(File dir) throws IOException {
@@ -38,8 +52,39 @@ public class MapImporterService {
         }
 
         File jsonFile = new File(zoneDir, "zone.json");
+        String content = "";
+
+        try {
+            content = FileUtils.readFile(jsonFile.getAbsolutePath(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            logger.warn("Cannot read zone.json: " + jsonFile.getAbsolutePath(), e);
+            return;
+        }
+
+        JSONObject json = new JSONObject(content);
+        String name = json.getString("name");
+
+        ZoneEntity zone = createAndGetZone(name);
 
         //TODO: add code here
+    }
+
+    private ZoneEntity createAndGetZone(String uniqueName) {
+        Optional<ZoneEntity> zoneEntityOptional = zoneDAO.findByName(uniqueName);
+        ZoneEntity zone = null;
+
+        if (!zoneEntityOptional.isPresent()) {
+            //create a new zone
+            logger.info("create new zone '{}'", uniqueName);
+
+            zone = new ZoneEntity(uniqueName);
+            zone = zoneDAO.save(zone);
+        } else {
+            //zone already exists
+            zone = zoneEntityOptional.get();
+        }
+
+        return zone;
     }
 
 }
