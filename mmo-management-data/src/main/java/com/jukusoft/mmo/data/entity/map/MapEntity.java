0d72c075181a6e2567ef46f0a5df38274464c873
JuKu <kuenzel.justin@t-online.de>
2020-06-06 17:36:35 +0200

    Change MapEntity

--- mmo-management-data/src/main/java/com/jukusoft/mmo/data/entity/map/MapEntity.java
+++ mmo-management-data/src/main/java/com/jukusoft/mmo/data/entity/map/MapEntity.java
@@ -1,12 +1,10 @@
 package com.jukusoft.mmo.data.entity.map;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.jukusoft.mmo.data.entity.AbstractEntity;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import javax.persistence.Cacheable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
@@ -20,6 +18,11 @@ import javax.validation.constraints.Size;
 @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class MapEntity extends AbstractEntity {
 
+    @ManyToOne(optional = false, cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id", nullable = false, updatable = false)//don't use an extra table, use join column instead
+    @JsonIgnore
+    private RegionEntity region;
+
     @Size(min = 2, max = 45)
     @Column(name = "name", unique = true, nullable = false, updatable = true)
     @NotEmpty(message = "name is required")
@@ -29,5 +32,27 @@ public class MapEntity extends AbstractEntity {
     @Column(name = "path", unique = true, nullable = false, updatable = true)
     @NotEmpty(message = "path is required")
     private String path;
+
+    public MapEntity(RegionEntity region, @Size(min = 2, max = 45) @NotEmpty(message = "name is required") String name, @Size(min = 2, max = 90) @NotEmpty(message = "path is required") String path) {
+        this.region = region;
+        this.name = name;
+        this.path = path;
+    }
+
+    protected MapEntity() {
+        //
+    }
+
+    public RegionEntity getRegion() {
+        return region;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
 
 }
--- mmo-management-data/src/main/java/com/jukusoft/mmo/data/entity/map/RegionEntity.java
+++ mmo-management-data/src/main/java/com/jukusoft/mmo/data/entity/map/RegionEntity.java
@@ -7,6 +7,9 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "game_regions", indexes = {
@@ -28,9 +31,14 @@ public class RegionEntity extends AbstractEntity {
     @NotEmpty(message = "name is required")
     private String name;
 
+    @OneToMany(mappedBy = "region", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @Column(name = "region")
+    private List<MapEntity> maps;
+
     public RegionEntity(ZoneEntity zone, @Size(min = 2, max = 45) @NotEmpty(message = "name is required") String name) {
         this.zone = zone;
         this.name = name;
+        this.maps = new ArrayList<>();
     }
 
     protected RegionEntity() {
@@ -43,6 +51,16 @@ public class RegionEntity extends AbstractEntity {
 
     public String getName() {
         return name;
+    }
+
+    public void addMap(MapEntity map) {
+        Objects.requireNonNull(map);
+        maps.add(map);
+    }
+
+    public void removeMap(MapEntity map) {
+        Objects.requireNonNull(map);
+        maps.remove(map);
     }
 
 }
