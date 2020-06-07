d69fc79408f4e8e2130914243aca22705730b389
JuKu <kuenzel.justin@t-online.de>
2020-06-07 17:11:58 +0200

    Add build script

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ git fetch --all
 git reset --hard origin/master
 git pull
 
+chmod 755 build.sh
+
 mvn clean install
 
 cp mmo-management-main/target/mmo-management-main-1.0.0-SNAPSHOT.jar ../bin/app.jar
