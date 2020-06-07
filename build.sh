d4956b390524d8582928003c33a0ff8fbf578a19
JuKu <kuenzel.justin@t-online.de>
2020-06-07 18:34:21 +0200

    Change build script

--- build.sh
+++ build.sh
@@ -9,14 +9,18 @@ git pull
 
 chmod 755 build.sh
 
+echo "Stop old screen session"
 ../bin/stopScreen.sh
 
+echo "Start build"
 mvn clean install
 
+echo "copy jars and configuration"
 cp -rf mmo-management-main/target/mmo-management-main-1.0.0-SNAPSHOT.jar ../bin/app.jar
 #cp -rf mmo-management-main/target/lib ../bin/lib
 
 # Copy configuration
 cp -rf ../config/application.properties ../bin/application.properties
 
+echo "Start new screen session"
 ../bin/startScreen.sh
