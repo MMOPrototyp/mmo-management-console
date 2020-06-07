c5486c67d985e1700c506894b6bf7918538bb60d
JuKu <kuenzel.justin@t-online.de>
2020-06-07 17:40:32 +0200

    Change build script

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ chmod 755 build.sh
 
 mvn clean install
 
-cp mmo-management-main/target/mmo-management-main-1.0.0-SNAPSHOT.jar ../bin/app.jar
-cp -rf mmo-management-main/target/lib ../bin/lib
+cp -rf mmo-management-main/target/mmo-management-main-1.0.0-SNAPSHOT.jar ../bin/app.jar
+#cp -rf mmo-management-main/target/lib ../bin/lib
 
 # Copy configuration
 cp ../config/application.properties ../bin/application.properties
